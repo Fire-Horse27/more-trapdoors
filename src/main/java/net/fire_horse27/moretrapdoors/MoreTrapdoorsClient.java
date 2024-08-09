cfe55fd203865bba528c0904a88e2d759f29e20d
Fire-Horse27 <firehorse2727@gmail.com>
2024-08-09 17:53:07 -0500

    Initial Commit

---
+++ src/main/java/net/fire_horse27/moretrapdoors/MoreTrapdoorsClient.java
@@ -0,0 +1,10 @@
+package net.fire_horse27.moretrapdoors;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreTrapdoorsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
