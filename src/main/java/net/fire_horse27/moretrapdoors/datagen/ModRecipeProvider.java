d4adb1e132aae2e19587d17d2c8ce52ed574b8d0
Fire-Horse27 <firehorse2727@gmail.com>
2024-08-09 19:00:35 -0500

    Generated Recipes

--- src/main/java/net/fire_horse27/moretrapdoors/datagen/ModRecipeProvider.java
+++ src/main/java/net/fire_horse27/moretrapdoors/datagen/ModRecipeProvider.java
@@ -3,9 +3,11 @@ package net.fire_horse27.moretrapdoors.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.TagKey;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -16,26 +18,34 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         super(output, registriesFuture);
     }
 
+    private static final List<Item> MATERIAL = List.of(Items.OAK_PLANKS, Items.SPRUCE_PLANKS, Items.BIRCH_PLANKS,
+            Items.JUNGLE_PLANKS, Items.ACACIA_PLANKS, Items.DARK_OAK_PLANKS, Items.MANGROVE_PLANKS, Items.CHERRY_PLANKS,
+            Items.BAMBOO_PLANKS, Items.CRIMSON_PLANKS, Items.WARPED_PLANKS, Items.COPPER_INGOT, Items.IRON_INGOT);
+
+    private static final List<Item> TRAPDOORS = List.of(Items.OAK_TRAPDOOR, Items.SPRUCE_TRAPDOOR, Items.BIRCH_TRAPDOOR,
+            Items.JUNGLE_TRAPDOOR, Items.ACACIA_TRAPDOOR, Items.DARK_OAK_TRAPDOOR, Items.MANGROVE_TRAPDOOR,
+            Items.CHERRY_TRAPDOOR, Items.BAMBOO_TRAPDOOR, Items.CRIMSON_TRAPDOOR, Items.WARPED_TRAPDOOR,
+            Items.COPPER_TRAPDOOR, Items.IRON_TRAPDOOR);
+
     @Override
     public void generate(RecipeExporter exporter) {
-        /*ShapelessRecipeJsonBuilder.create(RecipeCategory.REDSTONE, Items.DISPENSER)
-                .input(Items.DROPPER)
-                .input(Items.BOW)
-                .group("dispenser")
-                .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
-                .criterion(hasItem(Items.BOW), conditionsFromItem(Items.BOW))
-                .offerTo(exporter, new Identifier("dispenser_dropper"));*/
-        /*ShapedRecipeJsonBuilder.create(RecipeCategory.REDSTONE, Items.DISPENSER)
-                .pattern(" WS")
-                .pattern("WDS")
-                .pattern(" WS")
-                .input('S', Items.STRING)
-                .input('W', Items.STICK)
-                .input('D', Items.DROPPER)
-                .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
-                .group("dispenser")
-                .offerTo(exporter, new Identifier("dispenser_stackable"));*/
-        /*offerSmelting(exporter, Blocks.RAW_COPPER_BLOCK, RecipeCategory.BUILDING_BLOCKS, Blocks.COPPER_BLOCK,
-                        0.7f, 200, "iron_block");*/
+        for(int i = 0; i < 11; i++) {
+            ShapedRecipeJsonBuilder.create(RecipeCategory.REDSTONE, TRAPDOORS.get(i), 6)
+                    .pattern("###")
+                    .pattern("###")
+                    .group("wooden_trapdoor")
+                    .input('#', MATERIAL.get(i))
+                    .criterion(hasItem(MATERIAL.get(i)), conditionsFromItem(MATERIAL.get(i)))
+                    .offerTo(exporter);
+        }
+
+        for(int i = 11; i < 13; i++) {
+            ShapedRecipeJsonBuilder.create(RecipeCategory.REDSTONE, TRAPDOORS.get(i), 6)
+                    .pattern("###")
+                    .pattern("###")
+                    .input('#', MATERIAL.get(i))
+                    .criterion(hasItem(MATERIAL.get(i)), conditionsFromItem(MATERIAL.get(i)))
+                    .offerTo(exporter);
+        }
     }
 }
