83f2520465f6b71d7d2ff104adfda6ac5ee1330d
Fire-Horse27 <firehorse2727@gmail.com>
2025-01-19 22:56:23 -0500

    Updated to 1.21.4

--- src/main/java/net/fire_horse27/moretrapdoors/datagen/ModRecipeProvider.java
+++ src/main/java/net/fire_horse27/moretrapdoors/datagen/ModRecipeProvider.java
@@ -2,8 +2,8 @@ package net.fire_horse27.moretrapdoors.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.RecipeGenerator;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
@@ -20,11 +20,11 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     private static final List<Item> MATERIAL = List.of(Items.OAK_PLANKS, Items.SPRUCE_PLANKS, Items.BIRCH_PLANKS,
             Items.JUNGLE_PLANKS, Items.ACACIA_PLANKS, Items.DARK_OAK_PLANKS, Items.MANGROVE_PLANKS, Items.CHERRY_PLANKS,
-            Items.BAMBOO_PLANKS, Items.CRIMSON_PLANKS, Items.WARPED_PLANKS, Items.COPPER_INGOT, Items.IRON_INGOT);
+            Items.BAMBOO_PLANKS, Items.CRIMSON_PLANKS, Items.WARPED_PLANKS, Items.PALE_OAK_PLANKS, Items.COPPER_INGOT, Items.IRON_INGOT);
 
     private static final List<Item> TRAPDOORS = List.of(Items.OAK_TRAPDOOR, Items.SPRUCE_TRAPDOOR, Items.BIRCH_TRAPDOOR,
             Items.JUNGLE_TRAPDOOR, Items.ACACIA_TRAPDOOR, Items.DARK_OAK_TRAPDOOR, Items.MANGROVE_TRAPDOOR,
-            Items.CHERRY_TRAPDOOR, Items.BAMBOO_TRAPDOOR, Items.CRIMSON_TRAPDOOR, Items.WARPED_TRAPDOOR,
+            Items.CHERRY_TRAPDOOR, Items.BAMBOO_TRAPDOOR, Items.CRIMSON_TRAPDOOR, Items.WARPED_TRAPDOOR, Items.PALE_OAK_TRAPDOOR,
             Items.COPPER_TRAPDOOR, Items.IRON_TRAPDOOR);
 
     @Override
@@ -32,7 +32,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         return new RecipeGenerator(registries, exporter) {
             @Override
             public void generate() {
-                for (int i = 0; i < 11; i++) {
+                for (int i = 0; i < 12; i++) {
                     createShaped(RecipeCategory.REDSTONE, TRAPDOORS.get(i), 6)
                             .pattern("###")
                             .pattern("###")
@@ -42,7 +42,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                             .offerTo(exporter);
                 }
 
-                for (int i = 11; i < 13; i++) {
+                for (int i = 12; i < 14; i++) {
                     createShaped(RecipeCategory.REDSTONE, TRAPDOORS.get(i), 6)
                             .pattern("###")
                             .pattern("###")
