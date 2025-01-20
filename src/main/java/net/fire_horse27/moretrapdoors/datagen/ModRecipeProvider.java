6d12209b416a6160b5f36bb877dd828d04c7af6f
Fire-Horse27 <firehorse2727@gmail.com>
2025-01-20 15:52:11 -0500

    Changed Iron Bars Recipe to allow the trapdoor recipe to be consistent with other recipes

--- src/main/java/net/fire_horse27/moretrapdoors/datagen/ModRecipeProvider.java
+++ src/main/java/net/fire_horse27/moretrapdoors/datagen/ModRecipeProvider.java
@@ -18,21 +18,23 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         super(output, registriesFuture);
     }
 
+    private static final int WOODCOUNT = 12;
+
     private static final List<Item> MATERIAL = List.of(Items.OAK_PLANKS, Items.SPRUCE_PLANKS, Items.BIRCH_PLANKS,
             Items.JUNGLE_PLANKS, Items.ACACIA_PLANKS, Items.DARK_OAK_PLANKS, Items.MANGROVE_PLANKS, Items.CHERRY_PLANKS,
-            Items.BAMBOO_PLANKS, Items.CRIMSON_PLANKS, Items.WARPED_PLANKS, Items.PALE_OAK_PLANKS, Items.COPPER_INGOT, Items.IRON_INGOT);
+            Items.BAMBOO_PLANKS, Items.CRIMSON_PLANKS, Items.WARPED_PLANKS, Items.PALE_OAK_PLANKS);
 
     private static final List<Item> TRAPDOORS = List.of(Items.OAK_TRAPDOOR, Items.SPRUCE_TRAPDOOR, Items.BIRCH_TRAPDOOR,
             Items.JUNGLE_TRAPDOOR, Items.ACACIA_TRAPDOOR, Items.DARK_OAK_TRAPDOOR, Items.MANGROVE_TRAPDOOR,
-            Items.CHERRY_TRAPDOOR, Items.BAMBOO_TRAPDOOR, Items.CRIMSON_TRAPDOOR, Items.WARPED_TRAPDOOR, Items.PALE_OAK_TRAPDOOR,
-            Items.COPPER_TRAPDOOR, Items.IRON_TRAPDOOR);
+            Items.CHERRY_TRAPDOOR, Items.BAMBOO_TRAPDOOR, Items.CRIMSON_TRAPDOOR, Items.WARPED_TRAPDOOR,
+            Items.PALE_OAK_TRAPDOOR);
 
     @Override
     protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registries, RecipeExporter exporter) {
         return new RecipeGenerator(registries, exporter) {
             @Override
             public void generate() {
-                for (int i = 0; i < 12; i++) {
+                for (int i = 0; i < WOODCOUNT; i++) {
                     createShaped(RecipeCategory.REDSTONE, TRAPDOORS.get(i), 6)
                             .pattern("###")
                             .pattern("###")
@@ -42,14 +44,20 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                             .offerTo(exporter);
                 }
 
-                for (int i = 12; i < 14; i++) {
-                    createShaped(RecipeCategory.REDSTONE, TRAPDOORS.get(i), 2)
-                            .pattern("###")
-                            .pattern("###")
-                            .input('#', MATERIAL.get(i))
-                            .criterion(hasItem(MATERIAL.get(i)), conditionsFromItem(MATERIAL.get(i)))
-                            .offerTo(exporter);
-                }
+                createShaped(RecipeCategory.REDSTONE, Items.IRON_TRAPDOOR, 2)
+                    .pattern("###")
+                    .pattern("###")
+                    .input('#', Items.IRON_INGOT)
+                    .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                    .offerTo(exporter);
+
+                createShaped(RecipeCategory.MISC, Items.IRON_BARS, 16)
+                        .pattern("# #")
+                        .pattern("# #")
+                        .pattern("# #")
+                        .input('#', Items.IRON_INGOT)
+                        .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                        .offerTo(exporter);
             }
         };
     }
